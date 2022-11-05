8df83c1b04ebd1816c72c7ba3c55d516a5442e80
Ubuntu <tester@techpearl.com>
2022-11-05 17:19:36 +0530

    create mongo connection service

---
+++ services/mongo-connection-service.js
@@ -0,0 +1,10 @@
+const url = process.env.MONGODB_URI;
+const mongoose = require('mongoose');
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  });
